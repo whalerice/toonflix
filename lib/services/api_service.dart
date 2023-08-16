3b89de0b14d57f7531ea7fea24bb4f222320fd46
joyfulforest <>
2023-08-16 21:02:47 +0900

    fix

---
+++ lib/services/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstance = [];
+
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        webtoonInstance.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstance;
+    }
+    throw Error();
+  }
+}
