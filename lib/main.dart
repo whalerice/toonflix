e9af92e38b7dd7c758c4a83d35a24613eafa48ab
joyfulforest <>
2023-08-18 15:41:48 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
-import 'package:toonflix/services/api_service.dart';
+
+// image url 403 에러 나는경우
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
 
 void main() {
-  ApiService().getTodaysToons();
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MyApp());
 }
 
@@ -13,7 +25,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -1,8 +1,18 @@
 class WebtoonModel {
   final String title, thumb, id;
 
-  WebtoonModel.fromJson(Map<String, dynamic> json)
-      : title = json['titile'],
-        thumb = json['thumb'],
-        id = json['id'];
+  WebtoonModel({required this.title, required this.thumb, required this.id});
+
+  factory WebtoonModel.fromJson(Map<String, dynamic> data) {
+    return WebtoonModel(
+      id: data["id"],
+      title: data["title"],
+      thumb: data["thumb"],
+    );
+  }
+
+  // WebtoonModel.fromJson(Map<String, dynamic> json)
+  //     : title = json['titile'],
+  //       thumb = json['thumb'],
+  //       id = json['id'];
 }
